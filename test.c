573f23fbfc1b3cb0939711806539bdef1118547b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-06-06 22:45:50 -0700

    Update test.c

--- test.c
+++ test.c
@@ -46,13 +46,73 @@ void testHeapSort()
 	PrintArray(arr1, sz);
 }
 
+void testQuickSort()
+{
+	int arr1[] = { 5,456,2,3,476,23,-345,0, 24,12,45,8,4,7,9,-6,47 };
+	int sz = sizeof(arr1) / sizeof(int);
+	PrintArray(arr1, sz);
+	QuickSort(arr1, sz);
+	PrintArray(arr1, sz);
+}
+void testQuickSortNonR()
+{
+	int arr1[] = { 5,456,2,3,476,23,-345,0, 24,12,45,8,4,7,9,-6,47 };
+	int sz = sizeof(arr1) / sizeof(int);
+	PrintArray(arr1, sz);
+	QuickSortNonR(arr1,sz);
+	PrintArray(arr1, sz);
+}
+
+void testMergeSort()
+{
+	int arr1[] = { 5,456,2,3,476,23,-345,0, 24,12,45,8,4,7,9,-6,47};
+	int sz = sizeof(arr1) / sizeof(int);
+	PrintArray(arr1, sz);
+	MergeSort(arr1,sz);
+	PrintArray(arr1, sz);
+}
+
+
+void testMergeSortNonR()
+{
+	int arr1[] = { 5,456,2,3,476,23,-345,0, 24,12,45,8,4,7,9,-6,47 };
+	int sz = sizeof(arr1) / sizeof(int);
+	PrintArray(arr1, sz);
+	MergeSortNonR(arr1, sz);
+	PrintArray(arr1, sz);
+}
+
+void testCountSort()
+{
+	int arr1[] = { 5,0,1,5,4,2,2,2,1,3,2,0};
+	int sz = sizeof(arr1) / sizeof(int);
+	PrintArray(arr1, sz);
+	CountSort(arr1, sz);
+	PrintArray(arr1, sz);
+}
+
 
 int main()
 {
+	printf("testInsertSort :\n");
 	testInsertSort();
+	printf("testShellSort :\n");
 	testShellSort();
+	printf("testSelectSort :\n");
 	testSelectSort();
-	testBubbleSort(); 
+	printf("testBubbleSort :\n");
+	testBubbleSort();
+	printf("testHeapSort :\n");
 	testHeapSort();
+	printf("testQuickSort :\n");
+	testQuickSort();
+	printf("testQuickSortNonR :\n");
+	testQuickSortNonR();
+	printf("testMergeSort :\n");
+	testMergeSort();
+	printf("testMergeSortNonR :\n");
+	testMergeSortNonR();
+	printf("testCountSort :\n");
+	testCountSort();
 	return 0;
 }
